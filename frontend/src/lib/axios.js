89382aae4129407901832ffe9c1647e4a229da11
Ravi Kumar <04ravi.rk@gmail.com>
2025-02-27 16:31:51 +0530

    fatal error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "https://chatx-e8o4.onrender.com",
+    baseURL: "https://chatx-e8o4.onrender.com/api",
     // baseURL: "http://localhost:5001/api",
     withCredentials: true,
 })
