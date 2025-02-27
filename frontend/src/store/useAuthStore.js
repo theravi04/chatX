18f516c463c55ccb6a3939ea09ad1b83427f0c97
Ravi Kumar <04ravi.rk@gmail.com>
2025-02-27 16:45:01 +0530

    .

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios"
 import { toast } from "sonner"
 import { io } from "socket.io-client"
 
-const BASE_URL = "http://localhost:5001"
+const BASE_URL = "https://chatx-e8o4.onrender.com"
 
 export const useAuthStore = create((set, get) => ({
 
